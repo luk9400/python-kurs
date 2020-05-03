e71c61fa01dc7e2a9fdf9106785bcc7efd2cf2af
luk9400 <lukaszbratos@gmail.com>
2020-05-03 20:42:29 +0200

    list 5

--- list5/task1.py
+++ list5/task1.py
@@ -2,20 +2,68 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 
-def populate_matrix(df, users, m):
-    x = []
-    for i in users:
-        row = []
-        for j in range(2, m + 2):
-            j = df.loc[(df["userId"] == i) & (df["movieId"] == j)]["rating"].tolist()
-            if j == []:
-                row.append(0.0)
-            else:
-                row.append(*j)
-        x.append(row)
-    return np.array(x)
+def get_X(df, users, m):
+    X_data = df.loc[
+        (df.userId.isin(users)) & (df.movieId > 1) & (df.movieId <= m),
+        ["userId", "movieId", "rating"],
+    ].to_numpy()
+
+    X = np.zeros((len(users), m))
+    for userId, movieId, rating in X_data:
+        index = np.where(users == userId)[0][0]
+        X[index, int(movieId) - 2] = rating
+
+    return X
+
+
+def get_Y(df, users, m):
+    return df.loc[
+        (df["userId"].isin(users)) & (df["movieId"] == 1), ["rating"]
+    ].to_numpy()
+
+
+def first(df, users):
+    for m in [10, 100, 10000]:
+        y = get_Y(df, users, m)
+        x = get_X(df, users, m)
+
+        model = LinearRegression().fit(x, y)
+
+        predicted = plt.scatter(
+            [i for i in range(len(users))],
+            [model.predict([x[i]]) for i in range(len(users))],
+            alpha=0.5,
+            c="b",
+        )
+        real = plt.scatter(
+            [i for i in range(len(users))],
+            [y[i] for i in range(len(users))],
+            alpha=0.5,
+            c="r",
+        )
+
+        plt.legend((predicted, real), ["Predicted", "Real"])
+        plt.title(f"Predictions for {m} movies")
+        plt.show()
+
+
+def second(df, users):
+    for m in [10, 100, 200, 500, 1000, 10000]:
+        x = get_X(df, users, m)
+        y = get_Y(df, users, m)
+
+        test_x = x[:200]
+        test_y = y[:200]
+
+        model = LinearRegression().fit(test_x, test_y)
+
+        print("Prediction for", m, "movies:")
+        for i in range(200, 215):
+            print(i + 1, ":", *model.predict([x[i]]), "vs", y[i])
 
 
 def main():
@@ -24,16 +72,8 @@ def main():
     # list of users who rated ToyStory
     users = df.loc[df["movieId"] == 1]["userId"].tolist()
 
-    # list of our users' ToyStory ratings
-    tmp = df.loc[(df["userId"].isin(users)) & (df["movieId"] == 1)]["rating"].tolist()
-
-    y = []
-    for i in tmp:
-        y.append([i])
-
-    x = populate_matrix(df, users, 10)
-
-    print(np.linalg.lstsq(x, y))
+    first(df, users)
+    second(df, users)
 
 
 if __name__ == "__main__":
--- list5/task2.py
+++ list5/task2.py
@@ -3,31 +3,59 @@
 import numpy as np
 import pandas as pd
 
+np.seterr(invalid="ignore")
 
-def recommendation(x, y):
-    z = np.dot(x / np.linalg.norm(x, axis=0), y / np.linalg.norm(y))
 
-    X = x / np.linalg.norm(x, axis=0)
-    Z = z / np.linalg.norm(z)
+def get_recommendation(x, y):
+    z = np.dot(
+        np.nan_to_num(x / np.linalg.norm(x, axis=0)),
+        np.nan_to_num(y / np.linalg.norm(y)),
+    )
+
+    X = np.nan_to_num(x / np.linalg.norm(x, axis=0))
+    Z = np.nan_to_num(z / np.linalg.norm(z))
 
     return np.dot(X.T, Z)
 
 
+def get_ratings(df):
+    ratings_data = df.loc[
+        df["movieId"] < 10000, ["userId", "movieId", "rating"]
+    ].to_numpy()
+
+    ratings = np.zeros((611, 9019))
+    for data in ratings_data:
+        ratings[int(data[0]), int(data[1])] = data[2]
+    return ratings
+
+
+def get_movies(df):
+    return df.loc[df["movieId"] < 10000, ["movieId", "title"]].to_numpy()
+
+
 def main():
     df = pd.read_csv("./ml-latest-small/ratings.csv", sep=",")
-    df = df.loc[df["movieId"] < 10000]
+    movies_df = pd.read_csv("./ml-latest-small/movies.csv", sep=",")
 
+    ratings = get_ratings(df)
+    movies = get_movies(movies_df)
 
     my_ratings = np.zeros((9019, 1))
-    my_ratings[2571] = 5      # 2571 - Matrix
-    my_ratings[32] = 4        # 32 - Twelve Monkeys
-    my_ratings[260] = 5       # 260 - Star Wars IV
+    my_ratings[2571] = 5
+    my_ratings[32] = 4
+    my_ratings[260] = 5
     my_ratings[1097] = 4
 
+    recommendations = get_recommendation(ratings, my_ratings)
 
-    x = np.array([[5, 0, 2, 5], [4, 1, 2, 5]]).T
-    y = np.array([[4], [3]])
-    print(recommendation(x, y))
+    recommended_movies = []
+    for movie in movies:
+        recommended_movies.append((recommendations[movie[0]][0], movie[1]))
+
+    recommended_movies = sorted(recommended_movies, key=lambda x: x[0], reverse=True)
+
+    for rec in recommended_movies[:10]:
+        print(rec[0], "\t", rec[1])
 
 
 if __name__ == "__main__":
